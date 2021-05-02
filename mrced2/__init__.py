50f2323fe233258a3ff14753fab60fe5da0267d2
Martin Fenner <martin.fenner@datacite.org>
2021-05-02 12:21:32 +0200

    Finish initial version

--- mrced2/__init__.py
+++ mrced2/__init__.py
@@ -23,14 +23,19 @@ from .eventRecord import eventRecord
 from .evidenceRecords import evidenceRecords
 from .activityLogs import activityLogs
 
+# Function to query the REST API
+from .restApi import restApi
+
 # Function to get the last n months
+
+
 def lastNmonths(n: int) -> list:
     '''
     Find the first and last dates of the last n months. Returns a list of 
     ordered doubles, e.g. [('2017-12-01', '2017-12-31'), ('2018-01-01', '2018-01-31'),...]
-    
+
     Useful for building queries with monthly date ranges. e.g.
-    
+
     rng = t.last_n_months(30)
     query_template = "https://api.crossref.org/works?from-created-date={start}&until-created-date={end}"
     queries = [query_template.format(start=start, end=end) for start, end in rng]
@@ -48,9 +53,9 @@ def lastNmonths(n: int) -> list:
         in the format YYYY-MM-DD.
 
     '''
-    
+
     start = (datetime.utcnow().date() + DateOffset(months=-n)).date()
     return [
         (f"{d.year}-{d.month:02}-01", f"{d.year}-{d.month:02}-{d.day}")
         for d in pd.date_range(start, periods=n, freq="M")
-    ]   
+    ]
